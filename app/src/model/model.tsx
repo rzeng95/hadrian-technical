11704a94aaf841b334742217dfc3575326a819f0
Roland Zeng <roland.zeng@gmail.com>
2024-06-15 14:41:20 -0700

    pretty base file

--- app/src/model/model.tsx
+++ app/src/model/model.tsx
@@ -1,54 +1,48 @@
-import './model.css';
+import "./model.css";
 
-import * as React from 'react';
-import * as THREE from 'three';
-import { OrbitControls } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
-import { GLTFLoader } from 'three-stdlib';
+import * as React from "react";
+import * as THREE from "three";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { GLTFLoader } from "three-stdlib";
 
 interface ModelEntity {
-    bufferGeometry: THREE.BufferGeometry;
-    color: string;
+  bufferGeometry: THREE.BufferGeometry;
+  color: string;
 }
 
 export const Model = (): JSX.Element => {
-    const [modelEnts, setModelEnts] = React.useState<ModelEntity[]>([]);
+  const [modelEnts, setModelEnts] = React.useState<ModelEntity[]>([]);
 
-    React.useEffect(() => {
-        new GLTFLoader().load('./colored_glb.glb', gltf => {
-            const newModuleEntities: ModelEntity[] = [];
-            gltf.scene.traverse(element => {
-                if (element.type !== 'Mesh') return;
+  React.useEffect(() => {
+    new GLTFLoader().load("./colored_glb.glb", (gltf) => {
+      const newModuleEntities: ModelEntity[] = [];
+      gltf.scene.traverse((element) => {
+        if (element.type !== "Mesh") return;
 
-                const meshElement = element as THREE.Mesh;
-                newModuleEntities.push({
-                    bufferGeometry: meshElement.geometry as THREE.BufferGeometry,
-                    color: 'rgb(120, 120, 120)',
-                });
-            });
-            setModelEnts(newModuleEntities);
+        const meshElement = element as THREE.Mesh;
+        newModuleEntities.push({
+          bufferGeometry: meshElement.geometry as THREE.BufferGeometry,
+          color: "rgb(120, 120, 120)",
         });
+      });
+      setModelEnts(newModuleEntities);
+    });
+  }, []);
 
-    }, [])
-
-    return (
-        <div className="canvas-container">
-            <Canvas camera={{ position: [0, 0, 300] }} >
-                <ambientLight />
-                <OrbitControls makeDefault />
-                <group>
-                    {
-                        modelEnts.map((ent, index) => (
-                            <mesh
-                                geometry={ent.bufferGeometry}
-                                key={index}
-                            >
-                                <meshStandardMaterial color={ent.color} />
-                            </mesh>
-                        ))
-                    }
-                </group>
-            </Canvas>
-        </div>
-    )
+  return (
+    <div className="canvas-container">
+      <Canvas camera={{ position: [0, 0, 300] }}>
+        <ambientLight />
+        <OrbitControls makeDefault />
+        <group>
+          {modelEnts.map((ent, index) => (
+            <mesh geometry={ent.bufferGeometry} key={index}>
+              <meshStandardMaterial color={ent.color} />
+            </mesh>
+          ))}
+        </group>
+      </Canvas>
+    </div>
+  );
 };
